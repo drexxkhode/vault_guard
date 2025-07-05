436e4acd7fba533219cbaeb9a460e0deb553ff67
drexxkhode <Philipessien981@gmail.com>
2025-07-05 04:47:00 +0000

    helmet

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const helmet = require('helmet');
 dotenv.config();
 
 const app = express();
-app.use(helmet())
+app.use(helmet());
 app.use(express.json({ limit: '10mb' })); // Increase body size limit if needed
 app.use(express.urlencoded({ extended: true }));
 const port = 5000;
