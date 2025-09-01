2cdc7d3731a814d5a9a9437f7561344b48da307b
drexxkhode <Philipessien981@gmail.com>
2025-09-01 21:07:02 +0000

    actlog

--- backend/middleware/activityLogger.js
+++ backend/middleware/activityLogger.js
@@ -1,35 +1,48 @@
 const useragent = require("useragent");
-const logger =  require("../service/logger");
+const logger = require("../service/logger");
 const db = require("../db");
 
-function actLogger(event, description){
-return async function (req,res,next) {
-     try {
-        const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-    const agent = useragent.parse(req.headers['user-agent']);
-   const userId = "anonymous";
-   if(req.session?.user?.id){
-    userId= req.session.user.id;
-   }
-         const logData={
+function actLogger(event, description) {
+  return async function (req, res, next) {
+    try {
+      const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+      const agent = useragent.parse(req.headers["user-agent"]);
+
+      let userId = "anonymous";
+      if (req.session?.user?.id) {
+        userId = req.session.user.id;
+      }
+
+      const logData = {
         event,
         description,
         ip,
-        browser : agent.toAgent(),
+        browser: agent.toAgent(),
         os: agent.os.toString(),
         userId,
-        time: new Date ()
-    };
-    logger.info(logData);
-    sql = "INSERT INTO logs (event_type,description, ip, browser, os ,user_id, time) VALUES (?,?,?,?,?,?,?)";
-    await db.promise().query(sql,[logData.event, logData.description, logData.ip , logData.browser, logData.os,logData.userId, logData.time]);
-}
-      catch (err) {
-        logger.error(" Logging occured: " + err.message );
-     }
+        time: new Date(),
+      };
+
+      logger.info(logData);
+
+      const sql =
+        "INSERT INTO logs (event_type, description, ip, browser, os, user_id, time) VALUES (?,?,?,?,?,?,?)";
+      await db
+        .promise()
+        .query(sql, [
+          logData.event,
+          logData.description,
+          logData.ip,
+          logData.browser,
+          logData.os,
+          logData.userId,
+          logData.time,
+        ]);
+    } catch (err) {
+      logger.error("Logging error: " + err.message);
+    }
     next();
-    };
-
+  };
 }
 
-module.exports= actLogger;
+module.exports = actLogger;
