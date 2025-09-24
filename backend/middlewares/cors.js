2fe7919eb336e2ff1a2c2956d8392628bb2f0d1a
drexxkhode <Philipessien981@gmail.com>
2025-09-24 02:13:33 +0000

    CORS2

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,10 +1,10 @@
 const corsMiddleware = require("cors");
 
 const cors = corsMiddleware({
-origin: "https://vault-guard.onrender.com",
-credentials: true,
-methods: ['GET', 'PUT', 'POST','DELETE'],
-allowedHeaders: ['X-CSRF-Token']
-
+  origin: "https://vault-guard.onrender.com",
+  credentials: true,
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['X-CSRF-Token', 'Content-Type', 'Authorization', 'Accept']
 });
-module.exports= cors;
+
+module.exports = cors;
